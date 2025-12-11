83e83854c2e65a4e31f8e4c0fdf3b96065ab7fd7
takawauc <takawauc@student.42tokyo.jp>
2025-12-11 14:15:16 +0900

    ex02

---
+++ ex02/main.cpp
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: takawauc <takawauc@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/12/11 14:01:51 by takawauc          #+#    #+#             */
+/*   Updated: 2025/12/11 14:14:47 by takawauc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::string str = "HI THIS IS BRAIN";
+  std::string* stringPTR = &str;
+  std::string& stringREF = str;
+
+  std::cout << "address of \"str\"      : " << &str << "\n"
+            << "address of \"stringPTR\": " << &stringPTR << "\n"
+            << "address of \"stringREF\": " << &stringREF << "\n";
+  std::cout << "value of \"str\"      : " << str << "\n"
+            << "value of \"stringPTR\": " << *stringPTR << "\n"
+            << "value of \"stringREF\": " << stringREF << "\n";
+}
