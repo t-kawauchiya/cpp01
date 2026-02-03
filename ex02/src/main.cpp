64d52383759611b184896e568b131a9a39a1ba99
takawauc <takawauc@student.42tokyo.jp>
2026-02-03 15:30:06 +0900

    fix:ex02

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: takawauc <takawauc@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/11 14:01:51 by takawauc          #+#    #+#             */
-/*   Updated: 2025/12/11 14:14:47 by takawauc         ###   ########.fr       */
+/*   Updated: 2026/02/03 15:29:35 by takawauc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ int main()
   std::string& stringREF = str;
 
   std::cout << "address of \"str\"      : " << &str << "\n"
-            << "address of \"stringPTR\": " << &stringPTR << "\n"
+            << "address of \"stringPTR\": " << stringPTR << "\n"
             << "address of \"stringREF\": " << &stringREF << "\n";
   std::cout << "value of \"str\"      : " << str << "\n"
             << "value of \"stringPTR\": " << *stringPTR << "\n"
